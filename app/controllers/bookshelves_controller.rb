020994177483be16f0f7d267a915de299fe6ece1
Mike Farmer <mike.farmer@gmail.com>
2011-06-02 11:22:30 -0600

    Updated the bookshelves controller to work for new bookshelves.

--- app/controllers/bookshelves_controller.rb
+++ app/controllers/bookshelves_controller.rb
@@ -8,14 +8,17 @@ class BookshelvesController < ApplicationController
   end
 
   def new
+    @books = Book.all
     @bookshelf = Bookshelf.new
   end
 
   def create
     @bookshelf = Bookshelf.new(params[:bookshelf])
     if @bookshelf.save
+      sync_selected_books
       redirect_to @bookshelf, :notice => "Successfully created bookshelf."
     else
+      @books = Book.all
       render :action => 'new'
     end
   end
@@ -28,24 +31,10 @@ class BookshelvesController < ApplicationController
   def update
     @bookshelf = Bookshelf.find(params[:id])
     if @bookshelf.update_attributes(params[:bookshelf])
-
-      if params[:books]
-        checked_books = Book.where(:id => params[:books].keys).all
-        current_books = @bookshelf.books
-
-        books_to_remove = current_books - checked_books
-        books_to_add = checked_books - current_books
-
-        ChosenBook.delete_all(:bookshelf_id => @bookshelf.id, :book_id => books_to_remove)
-        @bookshelf.books << books_to_add
-      else
-        ChosenBook.delete_all(:bookshelf_id => @bookshelf.id)
-      end
-
-
-
+      sync_selected_books
       redirect_to @bookshelf, :notice  => "Successfully updated bookshelf."
     else
+      @books = Book.all
       render :action => 'edit'
     end
   end
@@ -55,4 +44,22 @@ class BookshelvesController < ApplicationController
     @bookshelf.destroy
     redirect_to bookshelves_url, :notice => "Successfully destroyed bookshelf."
   end
+
+  private 
+
+  def sync_selected_books
+    if params[:books]
+      checked_books = Book.where(:id => params[:books].keys).all
+      current_books = @bookshelf.books
+
+      books_to_remove = current_books - checked_books
+      books_to_add = checked_books - current_books
+
+      ChosenBook.delete_all(:bookshelf_id => @bookshelf.id, :book_id => books_to_remove)
+      @bookshelf.books << books_to_add
+    else
+      ChosenBook.delete_all(:bookshelf_id => @bookshelf.id)
+    end
+  end
+
 end
